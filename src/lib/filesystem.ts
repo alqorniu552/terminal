a1cc7643c79b33093bb4bbea6ab781e3f988bc33
Tirta Sadewa <tirtasadewa48@gmail.com>
2025-08-19 05:30:01 +0000

    Hapus [1;34mroot/[0m

--- src/lib/filesystem.ts
+++ src/lib/filesystem.ts
@@ -15,15 +15,5 @@ export type FilesystemNode = File | Directory;
 
 export const filesystem: Directory = {
   type: 'directory',
-  children: {
-    'root': {
-      type: 'directory',
-      children: {
-        'secret.txt': {
-            type: 'file',
-            content: 'This file is only accessible to the root user.'
-        }
-      }
-    }
-  },
+  children: {},
 };
