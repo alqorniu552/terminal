6e26a73b7dce57b4f899a649404d181dee381980
Tirta Sadewa <tirtasadewa48@gmail.com>
2025-08-21 08:57:21 +0000

    Perbaruhi agar tampilan kecil, seluler dan tablet dapat responsif dan bi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-code antialiased h-full">
+      <body className="h-full font-code antialiased">
         {children}
         <Toaster />
       </body>
